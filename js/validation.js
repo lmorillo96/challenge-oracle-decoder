dafc3e55d4b85efb831bcc820e717c3857892930
leandro_morillo <66586644+lmorillo96@users.noreply.github.com>
2022-01-26 12:21:51 -0500

    correct innerHTML validations

--- js/validation.js
+++ js/validation.js
@@ -1,23 +1,23 @@
 function validarEncrypt() {
     if (mensaje.value == ""){
        alert("I told you, write something");
-       mensaje.value = mensaje.innerHTML = "";
     }
 
    if (mensaje.value != mensaje.value.toLowerCase()) {
        alert("Without Mayus");
        mensajeEncriptado.value = mensajeEncriptado.innerHTML = "";
+       mensaje.value = mensaje.innerHTML = "";
     }
 }
 
 function validarDencrypt() {
     if (mensaje.value == ""){
         alert("Dude pls, you can't decrypt a Anything")
-        mensaje.value = mensaje.innerHTML = "";
     }
 
     if (mensaje.value != mensaje.value.toLowerCase()) {
         alert("Without Mayus");
         mensajeEncriptado.value = mensajeEncriptado.innerHTML = "";
+        mensaje.value = mensaje.innerHTML = "";
     }
 }
